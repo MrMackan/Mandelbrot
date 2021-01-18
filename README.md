089b2676905977d1bdb9b37c7d7c17fabdfc43d1
MrMackan <76006738+MrMackan@users.noreply.github.com>
2021-01-18 18:43:42 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# mandelbrot in java
---
+++ src/main/java/com/mrmackan/Mandelbrot.java
@@ -0,0 +1,12 @@
+package com.mrmackan;
+
+/**
+ * Mandelbrot set in  Java!
+ */
+public class Mandelbrot
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, Mandelbrot!");
+    }
+}
