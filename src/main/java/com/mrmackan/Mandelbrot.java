a3f4634aac07b15702a2070b816f7b74644d468b
KTheXIII <pratchaya.k.git@gmail.com>
2021-01-22 17:01:16 +0100

    Refactor code for multithreading

--- src/main/java/com/mrmackan/Mandelbrot.java
+++ src/main/java/com/mrmackan/Mandelbrot.java
@@ -12,59 +12,145 @@ import java.io.IOException;
 public class Mandelbrot
 {
     //picture size
-    private final int width = 1024;
-    private final int height = 1024;
+    private final int WIDTH = 1024;
+    private final int HEIGHT = 1024;
 
-    private final int maxIterations = 1000;
+    private final int MAX_ITERATIONS = 512;
 
-    private final double radius = 2;
-    private final double scale = 2;
+    private final double RADIUS = 2;
+    private final double SCALE = 2;
 
     //quick position changing
-    private final double posX = 0;
-    private final double posY = 0;
+    private final double POS_X = 0;
+    private final double POS_Y = 0;
 
     //quick color access
     private final double rgbRed = 40;
     private final double rgbGreen = 10;
     private final double rgbBlue = 5;
 
-    public Mandelbrot()
+    int mandelbrot[][];
+
+    BufferedImage bufferedImage;
+
+    class Worker implements Runnable
     {
-        int[][] mandelbrot = new int[height][width];
-        int k = (height * width) / 10;
-        int index;
-        double progress;
+        int width = 0;
+        int height = 0;
+        int rows;
+        int cols;
 
-        // Constructs a BufferedImage of one of the predefined image types.
-        BufferedImage bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        int startRow, endRow;
+        int startCol, endCol;
 
-        for (int y = 0; y < height; y++)
+        int data[][];
+
+        int iterations = 512;
+        double scale = 1.0;
+        double posX = 0, posY = 0;
+
+        Worker(int startRow, int endRow, int startCol, int endCol, int width, int height)
         {
-            var mapY_value = map(y, 0, height, -1, 1);
+            this.startRow = startRow;
+            this.endRow = endRow;
+            this.startCol = startCol;
+            this.endCol = endCol;
+            this.width = width;
+            this.height = height;
 
-            for (int x = 0; x < width; x++)
+            rows = endRow - startRow;
+            cols = endCol -startCol;
+
+            data = new int[rows][cols];
+        }
+
+        @Override
+        public void run()
+        {
+            compute();
+            System.out.printf("Thread: %d DONE!\n", Thread.currentThread().getId());
+        }
+
+        private void compute()
+        {
+            for (int y = 0; y < height; y++)
             {
-                var mapX_value = map(x, 0, width, -1, 1);
-                mandelbrot[y][x] = iteration(mapX_value + posX, mapY_value + posY);
+                var mapY_value = map(y, 0, height, -1, 1);
 
-                progress = ((double) (y * width + x) / (height * width));
-                index = (y * width + x);
-                if (index % k == 0)
-                { System.out.printf("Generating mandelbrot: %.0f%% \n", progress * 100); }
+                for (int x = 0; x < width; x++)
+                {
+                    var mapX_value = map(x, 0, width, -1, 1);
+                    data[y][x] = fractals(mapX_value + posX, mapY_value + posY, scale, iterations);
+                }
             }
         }
 
-        //sets random color for the png
-        for (int y = 0; y < height; y++)
+        private int fractals(double x, double y, double scale, int maxIterations)
         {
-            for (int x = 0; x < width; x++)
+            double z_Real = 0;
+            double z_Imaginary = 0;
+            double z_TempReal;
+
+            double c_Real = x * scale;
+            double c_Imaginary = y * scale;
+
+            int iterations;
+
+            for (iterations = 0; iterations < maxIterations; iterations++)
+            {
+                z_TempReal = (z_Real * z_Real) - (z_Imaginary * z_Imaginary) + c_Real;
+                z_Imaginary = (2 * z_Real * z_Imaginary) + c_Imaginary;
+                z_Real = z_TempReal;
+
+                if (Math.sqrt((z_Real * z_Real) + (z_Imaginary * z_Imaginary)) > RADIUS)
+                {
+                    break;
+                }
+            }
+
+            return iterations;
+        }
+    }
+
+    public Mandelbrot()
+    {
+        mandelbrot = new int[WIDTH][HEIGHT];
+
+        bufferedImage = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+
+        Worker w = new Worker(0, HEIGHT, 0, WIDTH, WIDTH, HEIGHT);
+        Thread t = new Thread(w);
+        t.start();
+
+        try {
+            t.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // TODO: Join the data when using multiple threads
+        mandelbrot = w.data;
+
+        generateImage();
+        save();
+    }
+
+    private void generateImage()
+    {
+        int k = (HEIGHT * WIDTH) / 10;
+        int index;
+        double progress;
+
+        //sets random color for the png
+        for (int y = 0; y < HEIGHT; y++)
+        {
+            for (int x = 0; x < WIDTH; x++)
             {
                 int count = mandelbrot[y][x];
 
-                if (count < maxIterations)
+                if (count < MAX_ITERATIONS)
                 {
-                    float n = ((float) count / maxIterations);
+                    float n = ((float) count / MAX_ITERATIONS);
                     float frequency = 3;
                     int red = (int) ((Math.sin(rgbRed * frequency * n - Math.PI / 2 + 1) * 0.5 + 0.5) * 255);
                     int green = (int) ((Math.sin(rgbGreen * frequency * n - Math.PI / 2 + 1) * 0.5 + 0.5) * 255);
@@ -75,13 +161,16 @@ public class Mandelbrot
                 }
                 else { bufferedImage.setRGB(x, y, 0); }
 
-                progress = ((double) (y * width + x) / (height * width));
-                index = (y * width + x);
+                progress = ((double) (y * WIDTH + x) / (HEIGHT * WIDTH));
+                index = (y * WIDTH + x);
                 if (index % k == 0)
                 { System.out.printf("Fixing colors: %.0f%% \n", progress * 100); }
             }
         }
+    }
 
+    private void save()
+    {
         // Saves mandelbrot as PNG
         File file = new File("mandelbrot.png");
         try
@@ -90,37 +179,6 @@ public class Mandelbrot
             ImageIO.write(bufferedImage, "png", file);
         }
         catch (IOException exception) { System.out.println("Failed to save"); }
-    }
-
-    /**
-     * math method for iterations
-     * @param x-coordinate
-     * @param y-coordinate
-     * @return amount of iterations
-     */
-    public int iteration(double x, double y)
-    {
-        double z_Real = 0;
-        double z_Imaginary = 0;
-        double z_TempReal;
-
-        double c_Real = x * scale;
-        double c_Imaginary = y * scale;
-
-        int iterations;
-
-        for (iterations = 0; iterations < maxIterations; iterations++)
-        {
-            z_TempReal = (z_Real * z_Real) - (z_Imaginary * z_Imaginary) + c_Real;
-            z_Imaginary = (2 * z_Real * z_Imaginary) + c_Imaginary;
-            z_Real = z_TempReal;
-
-            if (Math.sqrt((z_Real * z_Real) + (z_Imaginary * z_Imaginary)) > radius)
-            {
-                break;
-            }
-        }
-        return iterations;
     }
 
     public static double map(double input, double minIn, double maxIn, double minOut, double maxOut)
